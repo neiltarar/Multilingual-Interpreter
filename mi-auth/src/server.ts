19c6399854851fd15e68d39334a507935fe779ac
neil tarar <neiltarar@gmail.com>
2023-06-17 21:53:55 +1000

    working on the auth middleware

--- mi-auth/src/server.ts
+++ mi-auth/src/server.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
+import path from "path";
+import { authenticateToken } from "./middlewares/authMiddleware";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.AUTH_API_PORT;
 
+app.use(express.json()); // for parsing application/json
+app.use(express.static(path.join(__dirname, "build")));
+
+app.use("/users", userRoutes);
+
+app.get("/home", authenticateToken, (req, res) => {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.listen(PORT, () =>
 	console.log(`Server is running on http://localhost:${PORT}`)
 );
-
-app.use("/users", userRoutes);
