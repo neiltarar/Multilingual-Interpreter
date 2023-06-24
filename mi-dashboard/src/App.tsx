ceb83babed040000e79c4acdd647ec2f130ddfac
neil tarar <neiltarar@gmail.com>
2023-06-24 22:24:08 +1000

    Added the voice context

--- mi-dashboard/src/App.tsx
+++ mi-dashboard/src/App.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import SignIn from "./components/Auth/SignIn";
-// import SignUp from "./components/Auth/SignUp";
-import { AuthProvider } from "./contexts/AuthContext";
 import SignUp from "./components/Auth/SignUp";
+import { AuthProvider } from "./contexts/AuthContext";
+import { VoiceProvider } from "./contexts/VoiceContext";
 
 const App: React.FC = () => {
 	return (
 		<AuthProvider>
-			<Router>
+			<VoiceProvider>
 				<Routes>
 					<Route path='/signin' element={<SignIn />} />
 					<Route path='/signup' element={<SignUp />} />
 					<Route path='/' element={<Home />} />
 				</Routes>
-			</Router>
+			</VoiceProvider>
 		</AuthProvider>
 	);
 };
---
+++ mi-dashboard/src/contexts/VoiceContext.tsx
@@ -0,0 +1,77 @@
+import { createContext, useContext, useState, useRef } from "react";
+import axios from "axios";
+
+interface VoiceContextType {
+	//TODO define the properties and methods of your context value
+}
+
+const VoiceContext = createContext<VoiceContextType | undefined>(undefined);
+
+export const useVoice = () => {
+	return useContext(VoiceContext);
+};
+
+export const VoiceProvider: React.FC<{ children: React.ReactNode }> = ({
+	children,
+}) => {
+	const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+
+	const [transcription, setTranscription] = useState(null);
+
+	const handleButtonPress = async () => {
+		try {
+			const mediaStream = await navigator.mediaDevices.getUserMedia({
+				audio: true,
+			});
+			mediaRecorderRef.current = new MediaRecorder(mediaStream);
+
+			mediaRecorderRef.current.addEventListener(
+				"dataavailable",
+				async (event) => {
+					const recordedData = event.data;
+
+					// Create a FormData object to send the recorded data to the server
+					const formData = new FormData();
+					formData.append("recordedSound", recordedData, "recorded-sound.wav");
+
+					try {
+						// Send the recorded data to the server using Axios
+						await axios
+							.post("http://localhost:5000/api/upload", formData)
+							.then((res) => {
+								const transcriptedSpeech = res.data.message;
+								setTranscription(transcriptedSpeech);
+								console.log(transcriptedSpeech);
+							})
+							.catch((err) => console.error(err));
+
+						console.log("Recorded sound uploaded successfully");
+					} catch (error) {
+						console.error("Error uploading recorded sound:", error);
+					}
+				}
+			);
+
+			mediaRecorderRef.current.start();
+		} catch (error) {
+			console.error("Error accessing microphone:", error);
+		}
+	};
+
+	const handleButtonRelease = () => {
+		if (mediaRecorderRef.current) {
+			mediaRecorderRef.current.stop();
+		}
+	};
+
+	const value = {
+		transcription,
+		setTranscription,
+		handleButtonPress,
+		handleButtonRelease,
+	};
+
+	return (
+		<VoiceContext.Provider value={value}>{children}</VoiceContext.Provider>
+	);
+};
